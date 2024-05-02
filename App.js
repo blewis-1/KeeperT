2e7c290517edcae295f0c485698b6042365c6802
blewis-1 <lewisisraelb@gmail.com>
2024-05-01 20:22:05 -0400

    add input

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <AppBar></AppBar>
+      <InputTodo></InputTodo>
       <StatusBar style="auto" />
     </View>
   );
